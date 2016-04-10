46d19d053deb7ab731fe55fefd2829ca301a3b41
raphaelkyrdan <rafaelkyrdan@gmail.com>
2016-04-10 21:21:34 +0300

    Added new dependency  - Spray framework

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.10.4"
 
+resolvers += "spray repo" at "http://repo.spray.io"
+
 libraryDependencies ++= Seq (
   "com.firebase" % "firebase-client" % "1.0.18",
   "com.typesafe.akka" %% "akka-actor" % "2.3.9",
@@ -12,3 +14,8 @@ libraryDependencies ++= Seq (
   "com.twitter" % "hbc-core" % "2.2.0",
   "org.codehaus.jackson" % "jackson-core-asl" % "1.9.13"
 )
+
+//libraryDependencies += "io.spray" % "spray-can" % "1.1.3" // versions 1.1.x and 1.2.x
+libraryDependencies += "io.spray" %% "spray-can" % "1.3.3"
+
+
--- src/main/scala/nestapp/Main.scala
+++ src/main/scala/nestapp/Main.scala
@@ -5,18 +5,37 @@ package nestapp
   */
 
 import java.util.Properties
-
 import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
 
-object Main {
-  def main(args: Array[String]) {
-    val props = new Properties()
-    props.load(this.getClass.getClassLoader.getResourceAsStream("credentials.txt"))
-    val firebaseURL = props.getProperty("firebase-url")
-    val nestToken = props.getProperty("nest-token")
+//object Main {
+//  def main(args: Array[String]) {
+////    val props = new Properties()
+////    props.load(this.getClass.getClassLoader.getResourceAsStream("credentials.txt"))
+////    val firebaseURL = props.getProperty("firebase-url")
+////    val nestToken = props.getProperty("nest-token")
+////
+////    val system = ActorSystem("mySystem")
+////    system.actorOf(Props(new MyTopActor(firebaseURL, nestToken)))
+//
+//    implicit val system = ActorSystem()
+//
+//    // the handler actor replies to incoming HttpRequests
+//    val handler = system.actorOf(Props[Server], name = "handler")
+//
+//    IO(Http) ! Http.Bind(handler, interface = "localhost", port = 8080)
+//  }
+//}
 
-    val system = ActorSystem("mySystem")
-    system.actorOf(Props(new MyTopActor(firebaseURL, nestToken)))
-  }
+
+object Main extends App {
+
+  implicit val system = ActorSystem()
+
+  // the handler actor replies to incoming HttpRequests
+  val handler = system.actorOf(Props[Server], name = "handler")
+
+  IO(Http) ! Http.Bind(handler, interface = "localhost", port = 8080)
 }
 
